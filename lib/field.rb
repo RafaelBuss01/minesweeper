004a1ed7e5143f7e6865e0615a5ba4d10d3dffc8
Diego Marczal <dmarczal@gmail.com>
2011-09-30 13:30:40 -0300

    aula 02 - added some tests

--- lib/field.rb
+++ lib/field.rb
@@ -1,8 +1,33 @@
 class Field
   def initialize(m, n)
+    raise "Invalid size" unless m > 0 && n > 0
     @m, @n = m, n
+    @cells = Array.new
+    @m.times { |row| @cells[row] = ['.'] * @n }
   end
+
   def size
     [@m,@n]
+  end
+
+  def [](m, n)
+    @cells[m][n]
+  end
+
+  def bomb_at(m, n)
+    @cells[m][n] = '*'
+  end
+
+  def neighbours_of(m,n)
+    neighbours = []
+    (-1..1).each do |r|
+      (-1..1).each do |c|
+        next if r == 0 && c == 0
+        neighbour_x, neighbour_y = m+r, n+c
+        next if neighbour_x < 0 || neighbour_y < 0
+        neighbours << [neighbour_x, neighbour_y]
+      end
+    end
+    neighbours
   end
 end
--- spec/field_spec.rb
+++ spec/field_spec.rb
@@ -7,4 +7,50 @@ describe Field do
     Field.new(3,2).size.should == [3,2]
   end
 
+  it "should not create Field with invalid size" do
+    proc{ Field.new(-1,2) }.should raise_error "Invalid size"
+    proc{ Field.new(1,-2) }.should raise_error "Invalid size"
+  end
+
+  it "should initialize the Field with emptys cells" do
+    field = Field.new(2,2)
+    field[0,0].should == '.'
+    field[0,1].should == '.'
+    field[1,0].should == '.'
+    field[1,1].should == '.'
+  end
+
+  it "should allow placement of bombs" do
+    field = Field.new(3,3)
+    field.bomb_at(1,1)
+    field[1,1].should == '*'
+  end
+
+  describe "neighbours" do
+    it "should not include itself" do
+      field = Field.new(3,3)
+      field.neighbours_of(1,1).should_not include([1,1])
+    end
+
+    it "should detect neighbours of a cell in the middle" do
+      field = Field.new(3,3)
+
+      field.neighbours_of(1,1).should have(8).neighbours
+      field.neighbours_of(1,1).should include([0,0],[0,1],[0,2],
+                                              [1,0],      [1,2],
+                                              [2,0],[2,1],[2,2]
+                                              )
+    end
+
+    it "should eliminate neighbours outside of the Field (negative)" do
+      field = Field.new(3,3)
+
+      field.neighbours_of(0,0).should have(3).neighbours
+      field.neighbours_of(0,0).should include(      [0,1],
+                                              [1,0],[1,1])
+    end
+
+    it "should eliminate neighbours outside of the Field (positive)"
+  end
+
 end
